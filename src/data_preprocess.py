efbc17b40d8badbc2fa7e54639706a7e56c14f9b
subendhu19 <subendhu.iitm@gmail.com>
2020-03-26 16:29:31 -0400

    MFCC and LFB changes

--- src/data_preprocess.py
+++ src/data_preprocess.py
@@ -1,27 +1,39 @@
 import soundfile as sf
-from python_speech_features import logfbank
+from python_speech_features import logfbank, mfcc
 import os
 import pickle
 import argparse
 from tqdm import tqdm
+import numpy as np
 
 
-def compute_lfb(audio_data, sample_rate):
-    fbank_feat = logfbank(audio_data, sample_rate, nfilt=80)
-    return fbank_feat
+def compute_mfcc(audio_data, sample_rate, size):
+    mfcc_feat = mfcc(audio_data, sample_rate, numcep=size, nfilt=size*2)
+    return mfcc_feat - (np.mean(mfcc_feat, axis=0) + 1e-8)
 
 
-def get_lfb(audio_file):
+def compute_lfb(audio_data, sample_rate, size):
+    fbank_feat = logfbank(audio_data, sample_rate, nfilt=size)
+    return fbank_feat - (np.mean(fbank_feat, axis=0) + 1e-8)
+
+
+def get_mfcc(audio_file, size=13):
     audio, sample_rate = sf.read(audio_file)
-    feats = compute_lfb(audio, sample_rate)
+    feats = compute_mfcc(audio, sample_rate, size)
     return feats
 
 
-def process(path, split):
+def get_lfb(audio_file, size=26):
+    audio, sample_rate = sf.read(audio_file)
+    feats = compute_lfb(audio, sample_rate, size)
+    return feats
+
+
+def process(path, split, feature_type='lfb', size=26):
     root_folder = os.path.join(path, split)
     processed_set = []
     all_files = []
-    
+
     for folder in os.listdir(root_folder):
         if '.' not in folder:
             for subfolder in os.listdir(os.path.join(root_folder, folder)):
@@ -36,11 +48,14 @@ def process(path, split):
                 words = line.strip().split(' ')
                 transcription = ' '.join(words[1:])
                 audio_file = words[0] + '.flac'
-                audio_features = get_lfb(os.path.join(root_folder, folder, subfolder, audio_file))
+                if feature_type == 'lfb':
+                    audio_features = get_lfb(os.path.join(root_folder, folder, subfolder, audio_file), size=size)
+                else:
+                    audio_features = get_mfcc(os.path.join(root_folder, folder, subfolder, audio_file), size=size)
                 processed_set.append((os.path.join(root_folder, folder, subfolder, audio_file), audio_features,
                                       transcription))
 
-    pickle.dump(processed_set, open(os.path.join(path, split+'-13.p'), 'wb'))
+    pickle.dump(processed_set, open(os.path.join(path, '{}.{}.{}.p'.format(split, feature_type, size)), 'wb'))
     print('Processed {} set: {} instances'.format(split, len(processed_set)))
 
 
@@ -49,11 +64,15 @@ def main():
 
     parser.add_argument("--path", default=None, type=str, required=True,
                         help="The path to the data folder.")
+    parser.add_argument("--feature_type", default='lfb', type=str,
+                        help="Type of features for the audio: LFB or MFCC.")
+    parser.add_argument("--size", default=26, type=int,
+                        help="Size of the audio features.")
     args = parser.parse_args()
 
     splits = ['dev-clean', 'test-clean', 'train-clean-100', 'train-clean-360']
     for split in splits:
-        process(args.path, split)
+        process(args.path, split, args.feature_type, args.size)
 
 
 if __name__ == "__main__":
